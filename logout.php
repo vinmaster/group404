2f3cb206785e1fed2b069fcdb58d961701136a6a
Vincent Ho <yvho@csupomona.edu>
2013-04-06 11:47:55 -0700

    Change logout.php to redirect back to sign in page

--- logout.php
+++ logout.php
@@ -4,7 +4,7 @@ setcookie("login", "", time() - 60);
 
 session_start();
 session_destroy();
-// header('Location: index.php');
-echo '<a href="index.php">Home</a>';
+header('Location: index.php');
+// echo '<a href="index.php">Home</a>';
 
 ?>
